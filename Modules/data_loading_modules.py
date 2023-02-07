8ddbc60e85570fbfd1a27426da084a97cdcb1608
radioapple <104711470+radioapple@users.noreply.github.com>
2023-02-06 23:06:09 -0500

    Update data_loading_modules.py

--- Modules/data_loading_modules.py
+++ Modules/data_loading_modules.py
@@ -97,8 +97,8 @@ def extract_zipfile(file_path: str or Path,
     from pathlib import Path
             
     # --- Create directory, if name given ---
+    print(directory_name)
     if directory_name:
-        print(directory_name)
         dir_path = Path(directory_name)
         
         # Create directory, if it doesn't already exist
